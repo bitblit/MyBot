c3afb7ea86d725f489ce1414eb2890227681d0c7
Chris Weiss <bitblit@gmail.com>
2020-12-26 01:10:04 -0800

    Initial build

---
+++ src/blink/blink.ts
@@ -0,0 +1,19 @@
+// import * as pigpio from 'pigpio';
+import { Gpio } from 'pigpio';
+import { PromiseRatchet } from '@bitblit/ratchet/dist/common/promise-ratchet';
+
+// const Gpio = require('pigpio').Gpio;
+
+export class Blink {
+  private led: Gpio = new Gpio(17, { mode: Gpio.OUTPUT });
+
+  public async run(): Promise<void> {
+    for (let i = 0; i < 20; i++) {
+      for (let dutyCycle = 0; dutyCycle < 255; dutyCycle += 5) {
+        this.led.pwmWrite(dutyCycle);
+        await PromiseRatchet.wait(20);
+      }
+      await PromiseRatchet.wait(1500);
+    }
+  }
+}
