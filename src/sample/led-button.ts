a7457b0a959124ed732faf241ea480cb6a862ab0
Chris Weiss <bitblit@gmail.com>
2020-12-26 20:33:11 -0800

    Revert to 8.17.0

--- src/sample/led-button.ts
+++ src/sample/led-button.ts
@@ -4,14 +4,22 @@ import { Logger } from '@bitblit/ratchet/dist/common/logger';
 
 export class LedButton {
   private led: Gpio = new Gpio(4, { mode: Gpio.OUTPUT });
+  /*
   private button: Gpio = new Gpio(17, {
     mode: Gpio.INPUT,
     pullUpDown: Gpio.PUD_DOWN,
     edge: Gpio.EITHER_EDGE,
   });
 
+   */
+  private button: Gpio = new Gpio(17, {
+    mode: Gpio.INPUT,
+    pullUpDown: Gpio.PUD_UP,
+    alert: true,
+  });
+
   constructor() {
-    this.button.on('interrupt', (level) => {
+    this.button.on('alert', (level) => {
       Logger.info('%s Button %d', new Date(), level);
       this.led.digitalWrite(level);
     });
